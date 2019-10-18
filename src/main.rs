a6d6a86b7682df9fd614892def99a0a2f9141b41
Andy Roulin <andy.roulin@gmail.com>
2019-10-17 18:54:23 -0700

    sdl2: clear screen on render
    Without clearing the screen, visual glitches
    would appear when resizing the window.

--- src/main.rs
+++ src/main.rs
@@ -75,7 +75,7 @@ pub fn main() {
 
     let mut render = move |display: &Vec<Vec<u8>>| {
         canvas.set_draw_color(Color::RGB(0, 0, 0));
-        //canvas.clear();
+        canvas.clear();
         for i in 0..chip8::DISPLAY_HEIGHT {
             for j in 0..chip8::DISPLAY_WIDTH {
                 let color = if display[i][j] == 1 {
